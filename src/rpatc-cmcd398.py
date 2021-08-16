d97079b3c432c6ee3be04afd54dc653468fa8aba
CMCD1996 <connormcdowall@gmail.com>
2021-08-17 09:22:00 +1200

    Created source script

--- src/rpatc-cmcd398.py
+++ src/rpatc-cmcd398.py
@@ -1,4 +1,30 @@
 # Imports useful python packages
+# Essential
+import numpy as np # Arithmetic operations
+import pandas as pd # Data analysis package
+import matplotlib.pyplot as plt # Simple plotting
+import sklearn as skl # Simple statistical models 
+import tensorflow as tf # Tensorflow (https://www.tensorflow.org/)
+import csv as csv # read and write csvs
+# Additional
+import random as rd # random functionality
+import saspy as sas # Use saspy functionality in python
+import seaborn as sb # Imports seaborn library for use
+import wrds as wrds# Wharton Research Data Services API
+import pydatastream as pds # Thomas Reuters Datastream API
+import yfinance as yf # Yahoo Finance API
+import datetime as dt # Manipulate datetime values
+import statsmodels.api as sm # Create Stats functionalities
+import linearmodels as lp # Ability to use PooledOLS
+from sklearn.linear_model import LinearRegression
+from stargazer.stargazer import Stargazer
+import finance_byu as fin # Python Package for Fama-MacBeth Regressions
+from statsmodels.regression.rolling import RollingOLS # Use factor loadings
+from stargazer.stargazer import Stargazer
+import sympy as sy # convert latex code
+import scipy as sc # Scipy packages
+import tabulate as tb # Create tables in python
+import itertools as it # Find combinations of lists
 
 # Implements project script
 
