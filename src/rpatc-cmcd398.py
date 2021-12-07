0abef72c449cb42ccbd5ff6ae241a22d196197a7
CMCD1996 <connormcdowall@gmail.com>
2021-12-07 20:58:08 +0000

    Added new variables

--- src/rpatc-cmcd398.py
+++ src/rpatc-cmcd398.py
@@ -808,6 +808,28 @@ def autodiff_guide(example):
         # Print the outcomes of the simple model analysis
         for var, g in zip(layer.trainable_variables, grad):
             print(f'{var.name}, shape: {g.shape}')
+    if example == 'control_tape':
+        # A trainable variable
+        x0 = tf.Variable(3.0, name='x0')
+        # Not trainable
+        x1 = tf.Variable(3.0, name='x1', trainable=False)
+        # Not a Variable: A variable + tensor returns a tensor.
+        x2 = tf.Variable(2.0, name='x2') + 1.0
+        # Not a variable
+        x3 = tf.constant(3.0, name='x3')
+        with tf.GradientTape() as tape:
+            y = (x0**2) + (x1**2) + (x2**2)
+        grad = tape.gradient(y, [x0, x1, x2, x3])
+        for g in grad:
+            print(g)
+        [var.name for var in tape.watched_variables()]
+        x = tf.constant(3.0)
+        with tf.GradientTape() as tape:
+            tape.watch(x)
+            y = x**2
+        # dy = 2x * dx
+        dy_dx = tape.gradient(y, x)
+        print(dy_dx.numpy())
     return
 #################################################################################
 # Analytical/Calculus
