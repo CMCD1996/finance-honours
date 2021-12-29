aa739df5c37d807ac3425019f32f09d85d5808ee
CMCD1996 <connormcdowall@gmail.com>
2021-12-29 11:18:23 +0000

    Try function again

--- src/finance-honours.py
+++ src/finance-honours.py
@@ -772,10 +772,10 @@ def build_tensor_flow_model(train_dataset, val_dataset, test_dataset, model_name
             lf = custom_sharpe_ratio
         if selected_loss == 'custom_information_ratio': # loss = square(maximum(1 - y_true * y_pred, 0))
             lf = custom_information_ratio
-        if selected_loss == 'multi_layer_loss':
-            lf = multi_layer_loss
+        # if selected_loss == 'multi_layer_loss':
+        #     lf = multi_layer_loss
         if selected_loss == 'custom_loss':
-            lf = custom_loss
+            lf = custom_loss(reduction = red, name = 'custom_loss')
         #################################################################################
         # Metrics
         #################################################################################
