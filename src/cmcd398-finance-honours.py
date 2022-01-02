d7d66b71b60be73a2d913c059a435aadda375696
CMCD1996 <connormcdowall@gmail.com>
2022-01-02 09:33:34 +0000

    TRy hedge portfolio again

--- src/cmcd398-finance-honours.py
+++ src/cmcd398-finance-honours.py
@@ -1553,17 +1553,17 @@ class custom_hp(tf.keras.losses.Loss):
         y_true_flat = K.flatten(y_true)
         y_pred_flat = K.flatten(y_pred)
         # Calculates sum over vector tensors
-        y_true_sum = K.sum(y_true_flat)
-        y_pred_sum = K.sum(y_pred_flat)
+        y_true_sum = K.sum(y_true)
+        y_pred_sum = K.sum(y_pred)
         #
-        y_true_weights = (y_true_flat/y_true_sum)
-        y_pred_weights = (y_pred_flat/y_pred_sum)
+        y_true_weights = (y_true/y_true_sum)
+        y_pred_weights = (y_pred/y_pred_sum)
         # Transpose the weights
         y_true_transposed = K.transpose(y_true_weights)
         y_pred_transposed = K.transpose(y_pred_weights)
         # Multiply by the weights
-        y_true_loss = K.dot(K.flatten(y_true_transposed), y_true_flat)
-        y_pred_loss = K.dot(K.flatten(y_pred_transposed), y_pred_flat)
+        y_true_loss = K.dot(y_true_transposed, y_true_flat)
+        y_pred_loss = K.dot(y_pred_transposed, y_pred_flat)
         # multiplied = tf.keras.layers.Multiply()([x1, x2])
         loss = -1*(y_pred_loss)
         # loss = -1*(K.dot(K.transpose(tf.keras.layers.Lambda(lambda x: x /
