edc44ea558dbcdbdef6da93f5a5521989a71d0f6
CMCD1996 <connormcdowall@gmail.com>
2022-01-09 21:00:52 +0000

    Stage the changes

--- src/cmcd398-finance-honours.py
+++ src/cmcd398-finance-honours.py
@@ -1613,8 +1613,8 @@ def make_tensorflow_predictions(model_name, model_directory, selected_losses, da
         row_count = row_count + 1
         print('Completed row {} for all Loss functions.'.format(row_count))
         # Use the count to make sure the function is working properly (remove once tested)
-        if count == 5:
-            break
+        # if count == 5:
+        #     break
     for j in range(len(df_predictions)):
         print(df_predictions[j].info(verbose=True))
         print(df_predictions[j].head())
