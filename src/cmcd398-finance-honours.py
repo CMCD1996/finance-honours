486a50ca5fb4298beb60a82d8d6c5ff7797ba92c
CMCD1996 <connormcdowall@gmail.com>
2022-01-11 12:02:45 +0000

    Stage the typo changes

--- src/cmcd398-finance-honours.py
+++ src/cmcd398-finance-honours.py
@@ -915,8 +915,8 @@ def execute_conversion_options(model_name, selected_losses, hp_ols=False, pooled
                                replacement_text=replacement_text)
         if pooled_ols:
             for factor in factor_models:
-                fp_in = base_directory_in + 'pooled-ols/' + \
-                    factor + '/' + model_name + '-' + loss + '-' + factor + '.txt'
+                fp_in = base_directory_in + 'pooled-ols/' + factor + \
+                    '/' + model_name + '-' + loss + '-' + factor + '.txt'
                 fp_out = base_directory_out + model_name + '-' + loss + '-' + factor + '.tex'
                 convert_txt_to_tex(
                     fp_in, fp_out, replace_text=True, replacement_text=replacement_text)
