303ecf82fa5cab6ed38115d9f103426f48b47002
CMCD1996 <connormcdowall@gmail.com>
2022-01-13 13:38:53 +0000

    Custom metrics

--- src/cmcd398-finance-honours.py
+++ src/cmcd398-finance-honours.py
@@ -1909,7 +1909,7 @@ def create_learning_curves(model_name, model_directory, selected_losses, custom_
     for loss in selected_losses:
         if loss in ['mean_squared_error', 'custom_mse', 'custom_hp']:
             path = history_path + model_name + '-' + loss
-            model = joblib.load(path)
+            model = joblib.load(path, custom_object=custom_objects)
             # Create learning curves
             plt.plot(model.history['loss'], label=' Loss (Training)')
             plt.plot(model.history['val_loss'], label='Loss (validation)')
