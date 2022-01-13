361b6fca135c6bf977aebb17b12d56fa18ceacc2
CMCD1996 <connormcdowall@gmail.com>
2022-01-13 14:25:03 +0000

    Results

--- src/cmcd398-finance-honours.py
+++ src/cmcd398-finance-honours.py
@@ -1910,7 +1910,7 @@ def create_learning_curves(model_name, model_directory, selected_losses, custom_
     for loss in selected_losses:
         if loss in ['mean_squared_error', 'custom_mse', 'custom_hp']:
             path = history_path + model_name + '-' + loss
-            model = pickle.load(path, custom_object=custom_objects)
+            model = pickle.load(open(path), "rb")
             # Create learning curves
             plt.plot(model.history['loss'], label=' Loss (Training)')
             plt.plot(model.history['val_loss'], label='Loss (validation)')
@@ -2798,10 +2798,10 @@ analytical = False
 rank_functions = False
 # Model Building
 create_models = False
-make_predictions = True
-perform_regressions = True
+make_predictions = False
+perform_regressions = False
 # Output
-convert_text = True
+convert_text = False
 plot_learning_curves = False
 #################################################################################
 # Function Testing
